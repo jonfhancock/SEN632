b185ea091f98514f8260fa00c35ca6b723b1f17b
Jon Hancock <jonfhancock@gmail.com>
2011-08-30 14:17:11 -0700

    added initial Main classes

---
+++ Client/src/com/jonandcraig/client/Main.java
@@ -0,0 +1,13 @@
+package com.jonandcraig.client;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Moderator/src/com/jonandcraig/moderator/Main.java
@@ -0,0 +1,13 @@
+package com.jonandcraig.moderator;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
