62880f0a9661c53e9e1d1aa0a01b3889fdb8ca4d
merserc <craigmersereau@gmail.com>
2011-09-09 11:42:31 -0700

    Added some more methods to Moderator based on the Class diagram.  For some reason I can not get the import into Moderator to understand about Warrior

--- Moderator/src/com/apps4you/moderator/Moderator.java
+++ Moderator/src/com/apps4you/moderator/Moderator.java
@@ -1,12 +1,31 @@
 package com.apps4you.moderator;
 
+import com.apps4you.client.Warrior;
+
+import java.util.ArrayList;
+
+
+
 public class Moderator extends Server {
 
+private ArrayList warriorsList;
 
-	private static final long serialVersionUID = 1712048162291486001L;
+private static final long serialVersionUID = 1712048162291486001L;
 
 	public Moderator() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public void addOpponent(Warrior newWarrior)
+	{
+		this.warriorsList.add(newWarrior);
+	}
+	public void deleteOpponent(Warrior removeWarrior)
+	{
+		this.warriorsList.remove(removeWarrior);
+	}	
+	public ArrayList sendWarriorList()
+	{
+		return this.warriorsList;		
+	}
 }
