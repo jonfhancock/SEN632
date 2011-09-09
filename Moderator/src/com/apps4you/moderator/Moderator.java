8b124b13e81fae2e700a9b4951857fc545243ff9
merserc <craigmersereau@gmail.com>
2011-09-09 13:26:23 -0700

    A little more.  I am guessing I need to include Client in the reference for Moderator, but it is not allowing me to add the reference.  Jon Help!!!!

--- Moderator/src/com/apps4you/moderator/Moderator.java
+++ Moderator/src/com/apps4you/moderator/Moderator.java
@@ -3,6 +3,7 @@ package com.apps4you.moderator;
 import com.apps4you.client.Warrior;
 
 import java.util.ArrayList;
+import java.util.UUID;
 
 
 
@@ -16,6 +17,17 @@ private static final long serialVersionUID = 1712048162291486001L;
 		// TODO Auto-generated constructor stub
 	}
 	
+	private void moderateAttacks()
+	{
+		//determine the changes to the health of the warriors
+				
+	}
+	
+	private void sendAttackResults()
+	{
+		//TODO report back to the warriors the result of the attack		
+	}
+	
 	public void addOpponent(Warrior newWarrior)
 	{
 		this.warriorsList.add(newWarrior);
@@ -27,5 +39,11 @@ private static final long serialVersionUID = 1712048162291486001L;
 	public ArrayList sendWarriorList()
 	{
 		return this.warriorsList;		
+	}
+	
+	//Is the Warrior Id the ID of the attacked or the attackers
+	public void receiveAttackRequest(UUID warriorId)
+	{
+		//Use the warriorId to look up the
 	}
 }
