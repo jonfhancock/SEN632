82b60233689f7469280ec09a9765dbef05f67b7b
merserc <craigmersereau@gmail.com>
2011-09-09 06:58:19 -0700

    Added an Actions Enum to Warrior.  I not sure there is a reason to separate Attacks from Defenses at this point so I made it all an actions enum.

--- Client/src/com/apps4you/client/Warrior.java
+++ Client/src/com/apps4you/client/Warrior.java
@@ -51,4 +51,30 @@ public class Warrior extends Client{
 			return origin;
 		}
 	}
+	
+	public enum Actions {
+		
+		SLASH("Slash"),
+		STAB("Stab"),
+		BLOCK("Block"),
+		SIDESTEP("Sidestep"),
+		LASER("Laser"),
+		PARTICLEBEAM("Particle Beam"),
+		RAILGUN("Rail-Gun"),
+		SHIELD("Shield"),
+		PUNCH("Punch"),
+		COUNTERPUNCH("Counter Punch"),
+		KICK("Kick");		
+		
+		private final String chosenAction;
+		
+		private Actions (String actionChoice)
+		{
+			this.chosenAction = actionChoice;
+		}
+		public String getAction(){
+			return this.chosenAction;
+		}
+	}	
+	
 }
