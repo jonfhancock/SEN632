0780255c4865425938cdd85e155cff3a6ba0ca49
merserc <craigmersereau@gmail.com>
2011-09-14 08:00:36 -0700

    Cleaned Up UI.  Started on a connection button and dialog

--- Client/src/com/apps4you/client/Client.java
+++ Client/src/com/apps4you/client/Client.java
@@ -12,17 +12,13 @@ import java.util.UUID;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
+
 
 import com.apps4you.shared.*;
 
--- Client/src/com/apps4you/client/ClientUI.java
+++ Client/src/com/apps4you/client/ClientUI.java
@@ -1,35 +1,39 @@
 package com.apps4you.client;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 
+import javax.swing.border.EmptyBorder;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-import javax.swing.UIManager.LookAndFeelInfo;
+
 
 
 public class ClientUI extends JFrame implements ActionListener
 {
-   private JButton selectDataFileButton = new JButton("Select File");
-   private JButton opponentButton = new JButton("Opponent");
-   private JButton closeButton = new JButton("Close");
+
+   private static final long serialVersionUID = -8606655646939582216L;
+   private JPanel contentPane;
+   private final JButton connectButton = new JButton("Connect");
+   private final JButton selectDataFileButton = new JButton("Select File");
+   private final JButton opponentButton = new JButton("Opponent");
+   private final JButton closeButton = new JButton("Close");
    private JLabel welcomeLabel = new JLabel("Welcome to the Apps4You Client.");
    private JLabel moderatorCommentsLabel = new JLabel("Moderator Comments:");
    private JTextArea moderatorCommentsArea = new JTextArea();
    
    private Client client;
-   private final String  DEFAULT_HOST_LOCATION = "localhost";
-   private String host ;
+   public static final String  DEFAULT_HOST_LOCATION = "localhost";
+
    
    public ClientUI()
    {
@@ -37,45 +41,33 @@ public class ClientUI extends JFrame implements ActionListener
 	   
 	   setupFrame();
 	   
-	   //Need to add a button to connect to Server and allow for the 
-	   //user to select the server location
-	   host = DEFAULT_HOST_LOCATION;
-	   
-	   connectToServer(host);
-	   
+   
    }
    
-   private void connectToServer(String hostLocation)
+   protected void connectToServer(String hostLocation)
    {
 	   this.client = new Client(hostLocation);
 	   this.client.runClient();   
    }
    
-   private void lookAndFeelSetup()
-   {
-	   try {
-		    for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-		        if ("Nimbus".equals(info.getName())) {
-		        	UIManager.setLookAndFeel(info.getClassName());
-		        	System.out.println("\n Nimbus UI was selected.");
-		        	break;
-		            
-		        }
-		    }
-		} catch (Exception e) {
-		    // If Nimbus is not available, then use the system default.
-			System.out.println("\n Nimbus UI was not available \n");
-		}   
-   }
    
    private void setupFrame()
    {
-	   this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	   setBounds(100, 100, 450, 300);
+	   contentPane = new JPanel();
+	   contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+	   contentPane.setLayout(new BorderLayout(0, 0));
+	   setContentPane(contentPane);
+
 	   this.getContentPane().add(selectDataFileButton, BorderLayout.SOUTH);
 	   this.getContentPane().add(opponentButton, BorderLayout.SOUTH);
 	   this.getContentPane().add(closeButton, BorderLayout.SOUTH);
        this.getContentPane().add(welcomeLabel,BorderLayout.PAGE_START);
+       moderatorCommentsArea.setMinimumSize(new Dimension(200, 300));
+       moderatorCommentsArea.setText("Moderator Comments:");
        this.getContentPane().add(new JScrollPane(moderatorCommentsArea), BorderLayout.EAST);
+	   this.getContentPane().add(connectButton, BorderLayout.SOUTH);
 	   this.getContentPane().add(new JLabel(""),BorderLayout.SOUTH);
 	   
 	   this.setTitle("Apps4You - Client");
@@ -85,13 +77,17 @@ public class ClientUI extends JFrame implements ActionListener
 	   this.selectDataFileButton.addActionListener(this);
 	   this.closeButton.addActionListener(this);
 	   this.opponentButton.addActionListener(this);
-	   this.selectDataFileButton.setBounds(80, 60, 100, 30);
-	   this.closeButton.setBounds(80, 180, 100, 30);
-	   this.opponentButton.setBounds(80, 120, 100, 30);
-	   this.moderatorCommentsArea.setBounds(0, 200, 200, 30);
+	   this.connectButton.addActionListener(this);	
+	   this.connectButton.setBounds(80,40,100,30);	
+	   this.selectDataFileButton.setBounds(80, 100, 100, 30);
+	   this.opponentButton.setBounds(80, 160, 100, 30);
+	   this.closeButton.setBounds(80, 220, 100, 30);		   
+	   this.moderatorCommentsArea.setBounds(0, 200, 200, 30);	      
        	   
 	   this.setSize( 500, 300 ); // set size of window
 	   this.setVisible( true ); // show window
+	   this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		   
 	   
    }
    
@@ -118,6 +114,26 @@ public class ClientUI extends JFrame implements ActionListener
 	    else if (e.getSource() == opponentButton)
 	    {
 	    	System.out.println("\n Oppents Selection was chosen - Not implemented Yet. \n");
+	    	JOptionPane.showMessageDialog(this,
+	    		    "The Opponent button is not implemented, yet.",
+	    		    "Warning",
+	    		    JOptionPane.WARNING_MESSAGE);
+	    }
+
+	    else if (e.getSource() == connectButton)
+	    {
+	    	System.out.println("\n Connection Selection was chosen - Not implemented Yet. \n");
+	    	JOptionPane.showMessageDialog(this,
+	    		    "The Connection button is not implemented, yet.",
+	    		    "Warning",
+	    		    JOptionPane.WARNING_MESSAGE);	
+	    	/*
+	    	ConnectionDialog cd = new ConnectionDialog();
+	    	cd.setVisible(true);
+	    	connectToServer(cd.getHostTextField());
+	    	*/	 
+	     	 	
+   	
 	    }
 	    else if (e.getSource() == closeButton)
 	    {
@@ -125,4 +141,6 @@ public class ClientUI extends JFrame implements ActionListener
 	    	System.exit(0);
 	    }
    }
+   
+   
 }
