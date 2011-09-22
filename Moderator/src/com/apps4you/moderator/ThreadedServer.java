977015e4a51a670e4acf462b966029b1cb73036e
Jon Hancock <jonfhancock@gmail.com>
2011-09-21 22:42:44 -0700

    small addition to ThreadedServer

--- Moderator/src/com/apps4you/moderator/ThreadedServer.java
+++ Moderator/src/com/apps4you/moderator/ThreadedServer.java
@@ -1,5 +1,27 @@
 package com.apps4you.moderator;
 
-public class ThreadedServer {
+import java.io.IOException;
+import java.net.ServerSocket;
 
+
+public class ThreadedServer {
+	   private ServerSocket server; // server socket to connect with clients
+
+	   public ThreadedServer(){
+		   
+		      try
+		      {
+		         server = new ServerSocket( 12345, 2 ); // set up ServerSocket
+		      } // end try
+		      catch ( IOException ioException ) 
+		      {
+		         ioException.printStackTrace();
+		         System.exit( 1 );
+		      } // end catch
+	   }
+	
+	   public void execute()
+	   {
+	     
+	   } // end method execute
 }
