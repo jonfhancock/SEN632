5d37c6d8d2af5fa12f45cd941c6752400c1ba243
Jon Hancock <jonfhancock@gmail.com>
2011-09-22 15:46:05 -0700

    Added Logging conditionals to ClientCombatantUI

--- Client/src/com/apps4you/client/ClientCombatantUI.java
+++ Client/src/com/apps4you/client/ClientCombatantUI.java
@@ -1,6 +1,5 @@
 package com.apps4you.client;
 
-
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -36,297 +35,285 @@ import com.apps4you.shared.WarriorFactory;
 
 public class ClientCombatantUI extends JFrame {
 
-   private static final long serialVersionUID = -8606655646939582216L;
-   private JPanel contentPane;
-   private final JButton connectButton = new JButton("Server");
-   private final static JButton selectDataFileButton = new JButton("Select File");
-   private final static JButton opponentButton = new JButton("Opponent");
-   private final JButton newCombatantButton = new JButton("New");
-   private final JButton closeButton = new JButton("Close");
-   private final JButton actionButton = new JButton("Action");
-   private JLabel welcomeLabel = new JLabel("Welcome to the Apps4You Client.");
-   private static JTextArea moderatorCommentsArea = new JTextArea();
-   
-   private static Client client;
-   private static File file;
-   private static Warrior mWarrior;
-   private static Warrior mOpponent;
-   private static String hostLocation = "localhost";
-   private ArrayList<Warrior> opponents;
-   private final JPanel battlePanel = new JPanel();
-   private Actions battleAction;
+	private static final long serialVersionUID = -8606655646939582216L;
+	private JPanel contentPane;
+	private final JButton connectButton = new JButton("Server");
+	private final static JButton selectDataFileButton = new JButton(
+			"Select File");
+	private final static JButton opponentButton = new JButton("Opponent");
+	private final JButton newCombatantButton = new JButton("New");
+	private final JButton closeButton = new JButton("Close");
+	private final JButton actionButton = new JButton("Action");
+	private JLabel welcomeLabel = new JLabel("Welcome to the Apps4You Client.");
+	private static JTextArea moderatorCommentsArea = new JTextArea();
 
-   
-   protected void connectToServer()
-   {
-	   
-	   try{
-	        new Thread(
-	                new Runnable() {
-	                    public void run() {
-	                    	client = new Client(hostLocation);
-	    	            		Message message = new Message(mWarrior,Message.MessageCommand.NEWWARRIOR);
-								client.runClient(MessageFactory.toJSON(message));
+	private static Client client;
+	private static File file;
+	private static Warrior mWarrior;
+	private static Warrior mOpponent;
+	private static String hostLocation = "localhost";
+	private ArrayList<Warrior> opponents;
+	private final JPanel battlePanel = new JPanel();
+	private Actions battleAction;
 
-	                    }
-	                }).start();
-        	
-	     
-	   } catch(Exception e){
-		   displayText("There was an error connecting to the Moderator server.  Are you sure it is running?");
-	   }
-   }
+	protected void connectToServer() {
 
-   public void displayText(String text){
-	   moderatorCommentsArea.append(text);
-   }
+		try {
+			new Thread(new Runnable() {
+				public void run() {
+					client = new Client(hostLocation);
+					Message message = new Message(mWarrior,
+							Message.MessageCommand.NEWWARRIOR);
+					client.runClient(MessageFactory.toJSON(message));
 
-   private static ClientCombatantUI instance = null;
-   
-   public static ClientCombatantUI getInstance(){
-	   if(instance == null){
-		   instance = new ClientCombatantUI();
-	   }
-	   return instance;
-   }
-   
+				}
+			}).start();
+
+		} catch (Exception e) {
+			displayText("There was an error connecting to the Moderator server.  Are you sure it is running?");
+		}
+	}
+
+	public void displayText(String text) {
+		moderatorCommentsArea.append(text);
+	}
+
+	private static ClientCombatantUI instance = null;
+
+	public static ClientCombatantUI getInstance() {
+		if (instance == null) {
+			instance = new ClientCombatantUI();
+		}
+		return instance;
+	}
+
 	/**
 	 * Create the frame.
 	 */
 	private ClientCombatantUI() {
 		setResizable(false);
-    	opponents = new ArrayList<Warrior>();
+		opponents = new ArrayList<Warrior>();
 
-		   EventHandler handler = new EventHandler();
-		   
-	       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		   setBounds(100, 100, 450, 300);
-		   contentPane = new JPanel();
-		   contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		   setContentPane(contentPane);
-	       JScrollPane scrollPane = new JScrollPane();
-	       scrollPane.setBounds(272, 55, 212, 307);
-		   JLabel modCommentsLabel = new JLabel("Moderator Comments:");
-		   modCommentsLabel.setBounds(272, 30, 162, 20);
-		   
-		   setTitle("Apps4You - Client");
-		   closeButton.setBounds(64, 289, 100, 30);
-		   closeButton.addActionListener(handler);
-		   
-		   JPanel setupPanel = new JPanel();
-		   setupPanel.setBounds(5, 30, 234, 108);
-		   setupPanel.setLayout(null);
-		   setupPanel.setBorder(BorderFactory.createTitledBorder("Setup"));
-		   contentPane.setLayout(null);
-		   contentPane.add(closeButton);
-		   welcomeLabel.setBounds(5, 5, 474, 14);
-		   contentPane.add(welcomeLabel);
-		   contentPane.add(scrollPane);
-		   scrollPane.setViewportView(moderatorCommentsArea);
-		   moderatorCommentsArea.setMinimumSize(new Dimension(200, 300));
-		   moderatorCommentsArea.setLineWrap(true);
-		   contentPane.add(modCommentsLabel);
-		   contentPane.add(setupPanel);
-		   connectButton.setBounds(10, 26, 100, 30);
-		   setupPanel.add(connectButton);
-		   selectDataFileButton.setBounds(120, 26, 100, 30);
-		   setupPanel.add(selectDataFileButton);
-		   newCombatantButton.setBounds(69, 67, 100, 30);
-		   setupPanel.add(newCombatantButton);
-		   battlePanel.setBounds(5, 160, 234, 60);
-		   
-		   contentPane.add(battlePanel);
-		   battlePanel.setLayout(null);
-		   battlePanel.setBorder(BorderFactory.createTitledBorder("Battle"));
+		EventHandler handler = new EventHandler();
 
-		   opponentButton.setBounds(22, 30, 81, 23);
-		   battlePanel.add(opponentButton);
-		   opponentButton.addActionListener(handler);
-		   opponentButton.setEnabled(false);
-		   
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(272, 55, 212, 307);
+		JLabel modCommentsLabel = new JLabel("Moderator Comments:");
+		modCommentsLabel.setBounds(272, 30, 162, 20);
 
-		   actionButton.setEnabled(false);
-		   actionButton.setBounds(113, 30, 89, 23);
-		   battlePanel.add(actionButton);
-		   newCombatantButton.addActionListener(handler);
-		   selectDataFileButton.addActionListener(handler);
-		   connectButton.addActionListener(handler);	
-		   actionButton.addActionListener(handler);	
-		   
-		   setSize( 516, 400 ); // set size of window
-		   setVisible( true ); // show window
-		   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		   
-			   
+		setTitle("Apps4You - Client");
+		closeButton.setBounds(64, 289, 100, 30);
+		closeButton.addActionListener(handler);
+
+		JPanel setupPanel = new JPanel();
+		setupPanel.setBounds(5, 30, 234, 108);
+		setupPanel.setLayout(null);
+		setupPanel.setBorder(BorderFactory.createTitledBorder("Setup"));
+		contentPane.setLayout(null);
+		contentPane.add(closeButton);
+		welcomeLabel.setBounds(5, 5, 474, 14);
+		contentPane.add(welcomeLabel);
+		contentPane.add(scrollPane);
+		scrollPane.setViewportView(moderatorCommentsArea);
+		moderatorCommentsArea.setMinimumSize(new Dimension(200, 300));
+		moderatorCommentsArea.setLineWrap(true);
+		contentPane.add(modCommentsLabel);
+		contentPane.add(setupPanel);
+		connectButton.setBounds(10, 26, 100, 30);
+		setupPanel.add(connectButton);
+		selectDataFileButton.setBounds(120, 26, 100, 30);
+		setupPanel.add(selectDataFileButton);
+		newCombatantButton.setBounds(69, 67, 100, 30);
+		setupPanel.add(newCombatantButton);
+		battlePanel.setBounds(5, 160, 234, 60);
+
+		contentPane.add(battlePanel);
+		battlePanel.setLayout(null);
+		battlePanel.setBorder(BorderFactory.createTitledBorder("Battle"));
+
+		opponentButton.setBounds(22, 30, 81, 23);
+		battlePanel.add(opponentButton);
+		opponentButton.addActionListener(handler);
+		opponentButton.setEnabled(false);
+
+		actionButton.setEnabled(false);
+		actionButton.setBounds(113, 30, 89, 23);
+		battlePanel.add(actionButton);
+		newCombatantButton.addActionListener(handler);
+		selectDataFileButton.addActionListener(handler);
+		connectButton.addActionListener(handler);
+		actionButton.addActionListener(handler);
+
+		setSize(516, 400); // set size of window
+		setVisible(true); // show window
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
 	}
-	
 
-	
-	public void setWarrior(Warrior warrior){
+	public void setWarrior(Warrior warrior) {
 		mWarrior = warrior;
-        connectToServer();
-        welcomeLabel.setText("Connected as: "+ mWarrior.getName());
-        selectDataFileButton.setEnabled(false);
-        opponentButton.setEnabled(true);		
+		connectToServer();
+		welcomeLabel.setText("Connected as: " + mWarrior.getName());
+		selectDataFileButton.setEnabled(false);
+		opponentButton.setEnabled(true);
 	}
-	
-	public void haveBeenChosenForBattleSelectAction(Warrior whoSelectedMe)
-	{
-		System.out.println("Been Selected for Battle now choose Action");
+
+	public void haveBeenChosenForBattleSelectAction(Warrior whoSelectedMe) {
+		if(Consts.LOGGING){
+			System.out.println("Been Selected for Battle now choose Action");
+		}
 		Object[] possibilities = Actions.values();
-    	battleAction = (Actions)JOptionPane.showInputDialog(
-    			ClientCombatantUI.this,
-    	                    "You have been selected for battle.  Please select your battle action:",
-    	                    "Battle Selection Dialog",
-    	                    JOptionPane.QUESTION_MESSAGE,
-    	                    null,
-    	                    possibilities,
-    	                    null);	   
-    	System.out.println("\n The selected action as a defense was: " + battleAction.toString() ); 
-    	
-    	//I believe it should look more like this then the current sendData that is below
-    	Message message = new Message(mWarrior,Message.MessageCommand.DEFENSESELECTED,battleAction);
-		client.sendData(MessageFactory.toJSON(message));
+		battleAction = (Actions) JOptionPane
+				.showInputDialog(
+						ClientCombatantUI.this,
+						"You have been selected for battle.  Please select your battle action:",
+						"Battle Selection Dialog",
+						JOptionPane.QUESTION_MESSAGE, null, possibilities, null);
 		
-	}
+		if(Consts.LOGGING){
+			System.out.println("\n The selected action as a defense was: "
+					+ battleAction.toString());
+		}
 	
-	 private class EventHandler implements ActionListener
-	 {
+		
+		Message message = new Message(mWarrior,
+				Message.MessageCommand.DEFENSESELECTED, battleAction);
+		client.sendData(MessageFactory.toJSON(message));
+
+	}
+
+	private class EventHandler implements ActionListener {
 
 		@Override
-	   public void actionPerformed (ActionEvent e)
-	   {
-		 //Handle Select button action.
-		    if (e.getSource() == selectDataFileButton) 
-		    {	    	
-		        final JFileChooser fc = new JFileChooser();	
-		        final FileNameExtensionFilter fileFilter= new FileNameExtensionFilter("Warrior data file", "wdat");
-		        fc.setFileFilter(fileFilter);
-		        File f;
+		public void actionPerformed(ActionEvent e) {
+			// Handle Select button action.
+			if (e.getSource() == selectDataFileButton) {
+				final JFileChooser fc = new JFileChooser();
+				final FileNameExtensionFilter fileFilter = new FileNameExtensionFilter(
+						"Warrior data file", "wdat");
+				fc.setFileFilter(fileFilter);
+				File f;
 				try {
 					f = new File(new File(".").getCanonicalPath());
 					fc.setCurrentDirectory(f);
 				} catch (IOException e1) {
-					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-		        
-		        int returnVal = fc.showOpenDialog(ClientCombatantUI.this);
 
-		        if (returnVal == JFileChooser.APPROVE_OPTION) 
-		        {
-		        	file = fc.getSelectedFile();
-		            System.out.println("File " + file.getName() + " was selected.");
-		            setWarrior(Utils.readFileToCreateCombatant(file));
-            
-		        } else
-		        {
-		        	System.out.println("Open command cancelled by user./n");
-		        }	        
-		   }
-		    else if (e.getSource() == opponentButton)
-		    {	    	
-		    	ArrayList<Warrior> opponentsList = new ArrayList<Warrior>();
-		    	for(Warrior w: opponents){
-		    		if(!w.getWarriorId().equals(mWarrior.getWarriorId())){
-		    			opponentsList.add(w);
-		    		}
-		    	}
-		    	mOpponent= (Warrior)JOptionPane.showInputDialog(
-		    			ClientCombatantUI.this,
-		    	                    "Please select your opponent:",
-		    	                    "Opponent Selection Dialog",
-		    	                    JOptionPane.QUESTION_MESSAGE,
-		    	                    null,
-		    	                    opponentsList.toArray(),
-		    	                    null);	    	
-		    	System.out.println("\n"+mWarrior.getName()+" is prepared to engage " + mOpponent + " in battle!" ); 
+				int returnVal = fc.showOpenDialog(ClientCombatantUI.this);
 
-		    	ClientCombatantUI.this.actionButton.setEnabled(true);		
-		    	//TODO Update who was selected
-		    }
-		    else if (e.getSource() == actionButton)
-		    {
-		    	
-		    	Object[] possibilities = Actions.values();
-		    	battleAction = (Actions)JOptionPane.showInputDialog(
-		    			ClientCombatantUI.this,
-		    	                    "Please select your battle action:",
-		    	                    "Battle Selection Dialog",
-		    	                    JOptionPane.QUESTION_MESSAGE,
-		    	                    null,
-		    	                    possibilities,
-		    	                    null);	   
-		    	System.out.println("\n The selected action was: " + battleAction.toString() ); 
-		    	
-		    	//I believe it should look more like this then the current sendData that is below
-		    	Message message = new Message(mWarrior,Message.MessageCommand.BATTLEWARRIOR,battleAction, mOpponent);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					file = fc.getSelectedFile();
+					System.out.println("File " + file.getName()
+							+ " was selected.");
+					setWarrior(Utils.readFileToCreateCombatant(file));
+
+				} else {
+					if(Consts.LOGGING){
+						System.out.println("Open command cancelled by user./n");
+					}
+				}
+			} else if (e.getSource() == opponentButton) {
+				ArrayList<Warrior> opponentsList = new ArrayList<Warrior>();
+				for (Warrior w : opponents) {
+					if (!w.getWarriorId().equals(mWarrior.getWarriorId())) {
+						opponentsList.add(w);
+					}
+				}
+				mOpponent = (Warrior) JOptionPane.showInputDialog(
+						ClientCombatantUI.this, "Please select your opponent:",
+						"Opponent Selection Dialog",
+						JOptionPane.QUESTION_MESSAGE, null,
+						opponentsList.toArray(), null);
+				if(Consts.LOGGING){
+					System.out
+							.println("\n" + mWarrior.getName()
+									+ " is prepared to engage " + mOpponent
+									+ " in battle!");
+				}
+				ClientCombatantUI.this.actionButton.setEnabled(true);
+
+			} else if (e.getSource() == actionButton) {
+
+				Object[] possibilities = Actions.values();
+				battleAction = (Actions) JOptionPane
+						.showInputDialog(ClientCombatantUI.this,
+								"Please select your battle action:",
+								"Battle Selection Dialog",
+								JOptionPane.QUESTION_MESSAGE, null,
+								possibilities, null);
+				if(Consts.LOGGING){
+					System.out.println("\n The selected action was: "
+							+ battleAction.toString());
+				}
+				
+
+				Message message = new Message(mWarrior,
+						Message.MessageCommand.BATTLEWARRIOR, battleAction,
+						mOpponent);
 				client.sendData(MessageFactory.toJSON(message));
-					
-		    	ClientCombatantUI.this.selectDataFileButton.setEnabled(true);
-		    }
-		    else if (e.getSource() == connectButton)
-		    {
 
-		    	String s = (String)JOptionPane.showInputDialog(
-		    			ClientCombatantUI.this,
-		    	                    "Please enter the prefered server that you would like to connect to:",
-		    	                    "Prefered Server Dialog",
-		    	                    JOptionPane.QUESTION_MESSAGE,
-		    	                    null,
-		    	                    null,
-		    	                    "localhost");
+				selectDataFileButton.setEnabled(true);
+			} else if (e.getSource() == connectButton) {
 
-		    	//If a string was returned, say so.
-		    	if ((s != null) && (s.length() > 0)&& (s.length()<=15)) 
-		    	{
-		    		System.out.println("\n Prefered Server location was chosen - with a host of: " + s ); 
-		    		ClientCombatantUI.this.hostLocation = s;	    	
-			     	 
-		    	}
-		    	else
-		    	{
-		    		System.out.println("\n Prefered server location is not valid with: " + s + " as a value.");	 
-		    	}
-	   	
-		    }
-		    else if (e.getSource() == newCombatantButton)
-		    {
-		    	CreateWarrior dialog = new CreateWarrior();
+				String s = (String) JOptionPane
+						.showInputDialog(
+								ClientCombatantUI.this,
+								"Please enter the prefered server that you would like to connect to:",
+								"Prefered Server Dialog",
+								JOptionPane.QUESTION_MESSAGE, null, null,
+								"localhost");
+
+				// If a string was returned, say so.
+				if ((s != null) && (s.length() > 0) && (s.length() <= 15)) {
+					if(Consts.LOGGING){
+						System.out
+								.println("\n Prefered Server location was chosen - with a host of: "
+										+ s);
+					}
+					hostLocation = s;
+
+				} else {
+					if(Consts.LOGGING){
+						System.out
+								.println("\n Prefered server location is not valid with: "
+										+ s + " as a value.");
+					}
+				}
+
+			} else if (e.getSource() == newCombatantButton) {
+				CreateWarrior dialog = new CreateWarrior();
 				dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 				dialog.setVisible(true);
-				
-				
-		    }
-		    else if (e.getSource() == closeButton)
-		    {
-		    	if ((client != null)&&(mWarrior !=null))
-		    	{
-		    	  if(file == null){
-		    		  file = new File(mWarrior.getName()+".wdat");
-		    	  }
-		    	  Utils.saveWarriorToFile(mWarrior, file);
-		    	}
-		    	dispose();
-		    	System.exit(0);
-		    }
-	     } 
-	 }
-	 
-	 public void addOpponent(Warrior opponent){
-		 opponents.add(opponent);
-	 }
-	 public void removeOpponent(Warrior opponent){
-		 opponents.remove(opponent);
-	 }
-	 public void setOpponents(ArrayList<Warrior> opponentsList){
-//		 for(Warrior w: opponentsList){
-//			 if(w.getWarriorId().equals(mWarrior.getWarriorId())){
-//				 opponentsList.remove(w);
-//			 }
-//		 }
-		 opponents = opponentsList;
-		
-	 }
-	 
-  }
+
+			} else if (e.getSource() == closeButton) {
+				if ((client != null) && (mWarrior != null)) {
+					if (file == null) {
+						file = new File(mWarrior.getName() + ".wdat");
+					}
+					Utils.saveWarriorToFile(mWarrior, file);
+				}
+				dispose();
+				System.exit(0);
+			}
+		}
+	}
+
+	public void addOpponent(Warrior opponent) {
+		opponents.add(opponent);
+	}
+
+	public void removeOpponent(Warrior opponent) {
+		opponents.remove(opponent);
+	}
+
+	public void setOpponents(ArrayList<Warrior> opponentsList) {
+		opponents = opponentsList;
+	}
+
+}
