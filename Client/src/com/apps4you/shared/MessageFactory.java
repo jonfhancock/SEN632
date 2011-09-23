210edfaba0e299296ce0f85fc9e4dda3b9905b77
Jon Hancock <jonfhancock@gmail.com>
2011-09-22 21:45:30 -0700

    Commented MessageFactory

--- Client/src/com/apps4you/shared/MessageFactory.java
+++ Client/src/com/apps4you/shared/MessageFactory.java
@@ -8,17 +8,27 @@ import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 
 /**
+ * Marshal Message objects to JSON strings and parse JSON strings back out to
+ * Message objects.
  * 
  * @author jonfhancock
- *
+ * 
  */
 public class MessageFactory {
-	private static ObjectMapper mapper = new ObjectMapper(); 
+	private static ObjectMapper mapper = new ObjectMapper();
 
-	public static String toJSON(Message message){
+	/**
+	 * Get a JSON string for any Message object
+	 * 
+	 * @param message
+	 *            Any Message object that needs to be serialized to JSON
+	 * @return a JSON encoded string containing all the details of the given
+	 *         message.
+	 */
+	public static String toJSON(Message message) {
 		String returnString = null;
 		try {
-			returnString =  mapper.writeValueAsString(message);
+			returnString = mapper.writeValueAsString(message);
 		} catch (JsonGenerationException e) {
 			e.printStackTrace();
 		} catch (JsonMappingException e) {
@@ -28,10 +38,20 @@ public class MessageFactory {
 		}
 		return returnString;
 	}
-	
-	public static Message fromJSON(String jsonString){
+
+	/**
+	 * Build a Message object from a JSON string
+	 * 
+	 * @param jsonString
+	 *            a correctly formatted, serialized Message Object as a JSON
+	 *            string. You should have used MessageFactory.toJSON() to
+	 *            generate this string.
+	 * @return A Message object containing all the details that were encoded
+	 *         into the JSON string.
+	 */
+	public static Message fromJSON(String jsonString) {
 		Message message = null;
-		
+
 		try {
 			message = mapper.readValue(jsonString, Message.class);
 		} catch (JsonParseException e) {
