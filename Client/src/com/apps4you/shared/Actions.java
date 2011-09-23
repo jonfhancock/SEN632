89cc702ca766986cd5c3b5fc28e5fac04f1061fb
Jon Hancock <jonfhancock@gmail.com>
2011-09-22 20:55:58 -0700

    Commented Actions and changed the actions to match the Hitchiker's Guide theme in Origins.

--- Client/src/com/apps4you/shared/Actions.java
+++ Client/src/com/apps4you/shared/Actions.java
@@ -1,30 +1,46 @@
 package com.apps4you.shared;
 
+/**
+ * The battle actions used when attacking or defending against another Warrior
+ * 
+ * @author jonfhancock
+ * 
+ */
 public enum Actions {
-	
-	SLASH("Slash"),
-	STAB("Stab"),
-	BLOCK("Block"),
-	SIDESTEP("Sidestep"),
-	LASER("Laser"),
-	PARTICLEBEAM("Particle Beam"),
-	RAILGUN("Rail-Gun"),
-	SHIELD("Shield"),
-	PUNCH("Punch"),
-	COUNTERPUNCH("Counter Punch"),
-	KICK("Kick");		
-	
+
+	BLOCK("Block"), 
+	HIDEUNDERTOWEL("Hide under Towel"),
+	KICK("Kick"), 
+	KILOZAPGUN("Kill-o-Zap Pistol"), 
+	LASER("Laser"), 
+	PARTICLEBEAM("Particle Beam"), 
+	POINTOFVIEWGUN("Point of View Gun"), 
+	PUNCH("Punch"), 
+	SEP("Someone Else's Problem Field"), 
+	WHIPWITHTOWEL("Whip with Towel");
+
 	private final String chosenAction;
-	
-	private Actions (String actionChoice)
-	{
+
+	private Actions(String actionChoice) {
 		this.chosenAction = actionChoice;
 	}
-	public String getAction(){
+
+	/**
+	 * Get the enum name of an action
+	 * 
+	 * @return the enum name of an action as a String
+	 */
+	public String getAction() {
 		return this.chosenAction;
 	}
+
+	/**
+	 * Get the user friendly value of the action
+	 * 
+	 * @return the plain value of the enum
+	 */
 	@Override
-	public String toString(){
+	public String toString() {
 		return this.chosenAction;
 	}
-}	
+}
